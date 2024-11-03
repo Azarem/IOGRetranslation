72a3f7d9a32213b33b68acb32ee91fa93569b876
viewtifulslayer <viewtifulslayer@gmail.com>
2024-11-03 18:27:20 -0500

    Add files via upload
    Adjusted logo and partially localized chapter 1.

--- patches/chapter1.asm
+++ patches/chapter1.asm
@@ -47,7 +47,7 @@ charstring_0BD272 ~However, among[N]those, not one[N]had noticed the[N]tremendou
 ----------------------
 ?INCLUDE 'sc08_priest'
 ----------------------
-widestring_048AE9 `[TPL:0][TPL:8][DLY:0]My name is Tim.[FIN][TPL:B]Exactly one year has[N]passed since I went on an[N]expedition to the Tower[N]of Babel with my dad.[FIN]Of the members of my[N]dad's team, only I was[N]able to survive and return[N]to this town in safety…[FIN]Even now, I can't believe[N]that dad died or anything[N]like that. No, I don't[N]even intend to believe it.[FIN]When I grow up or so, I[N]intend to become an[N]explorer and charge[N]around the world.[FIN]Because if that happens,[N]I believe that I'll be[N]able to meet my dad[N]somewhere…[END]`
+widestring_048AE9 `[TPL:0][TPL:8][DLY:0]My name is Tim.[FIN][TPL:B]Exactly one year has[N]passed since I went on an[N]expedition to the Tower[N]of Babel with my dad.[FIN]Of the members of his[N]team, only I was able[N]to survive and safely[N]return to this town…[FIN]I still can't believe[N]that dad died or anything[N]like that. I don't think[N]I'll ever believe it…[FIN]When I grow up, I'm going[N]to become an explorer like[N]him and travel around[N]around the world.[FIN]Because when that happens,[N]I believe that somewhere,[N]I'll find my dad…[END]`
 ;My name is Tim.
 ;Exactly one year has passed since I went on an expedition to the Tower of Babel with my dad.
 ;Of the members of my dad's team, only I was able to survive and return to this town in safety…
@@ -55,7 +55,7 @@ widestring_048AE9 `[TPL:0][TPL:8][DLY:0]My name is Tim.[FIN][TPL:B]Exactly one y
 ;When I grow up or so, I intend to become an explorer and charge around the world.
 ;Because if that happens, I believe that I'll be able to meet my dad somewhere…
 
-widestring_048C01 `[TPL:16][TPL:D]Priest: Today's[N]lesson ends here.[FIN][TPL:E]The four of you do your[N]best so as not to stay[N]behind next time.[FIN][TPL:F]Aside from that,[N]things resembling[N]monsters have recently[N]been seen outside of town.[FIN]When you leave,[N]please go with a parent.[END]`
+widestring_048C01 `[TPL:16][TPL:D]Priest: Today's lesson[N]ends here.[FIN][TPL:F]The four of you do your[N]best so that you won't[N]have to stay behind after[N]classes next time.[FIN]Furthermore, creatures[N]resembling monsters have[N]recently been spotted[N]outside of town.[FIN]When you leave, please[N]be sure to have a parent[N]accompany you.[END]`
 ;Priest: Today's lesson ends here.
 ;The four of you do your best so not to stay behind next time.
 ;Aside from that, things that look like monsters have recently been being seen outside of town. When you leave, please go with a parent.
@@ -70,7 +70,7 @@ widestring_048C98 `[TPL:16][TPL:D]Ah, Tim.[N]Please pray with me.[FIN][TPL:E]O G
 widestring_048DD1 `[TPL:5][TPL:9]Morris: Alright, today[N]too, same place as always![END]`
 ;Morris: Alright, today too, same place as always!
 
-widestring_048E00 `[TPL:3][TPL:9]Eric: I'm gonna stop at[N]home first, then I'll go.[FIN][TPL:B]If I don't go home[N]quickly, the fact that I[N]had to stay behind for[N]make-up lessons is going[WAI][N]to leak to mommy… Heheh…[END]`
+widestring_048E00 `[TPL:3][TPL:9]Eric: I'm gonna stop at[N]home first, then I'll go.[FIN][TPL:B]If I don't go home right[N]away, mommy's going to[N]find out that I had to[N]stay behind for make-up[WAI][N]lessons again… Heh heh…[END]`
 ;Eric: I'm stop off at home, then I'll go.
 ;If I don't go home quickly, the fact that I had to stay behind for make-up lessons is going to leak to mommy… Heheh…
 
@@ -105,26 +105,26 @@ widestring_08DE43 `[CLR][PAL:4]Then thou shouldst go.[END]`
 ------------------------
 ?INCLUDE 'sc01_roof_man'
 ------------------------
-widestring_0484B7 `[TPL:16][TPL:D]Hey, Tim… You shouldn't[N]be coming up here…[FIN][TPL:F]I think you have a[N]habit of jumping down[N]from various places,[N]though. I can't help[WAI][N]but worry. Aww.[END]`
+widestring_0484B7 `[TPL:16][TPL:D]Hey, Tim… You shouldn't[N]be coming up here…[FIN][TPL:F]I know I can't stop you[N]from jumping down from[N]high places like these,[N]but I can't help worrying![END]`
 ;Hey, Tim… You shouldn't be coming up here…
 ;I think you have a habit of jumping down from various places, though. I can't help but worry. Aww.
 
 ------------------------
 ?INCLUDE 'sc01_sprint_man'
 ------------------------
-widestring_04923D `[TPL:16][TPL:F]You seem like you've[N]got pretty fast feet, huh.[N]You run when you mash the[N]direction button twice.[END]`
+widestring_04923D `[TPL:16][TPL:F]You seem like you've got[N]pretty fast feet, huh.[N]Have you tried sprinting?[N]Give it a try sometime![FIN][TPL:F]Mashing the direction[N]button twice or pressing[N]the sprint button while[N]walking will both work.[END]`
 ;You seem like you've got pretty fast feet, huh. You run when you mash the direction button twice.
 
 ------------------------
 ?INCLUDE 'jeweler_gem'
 ------------------------
-widestring_08D094 `[TPL:16][TPL:F]I am Gem, a jewel[N]merchant that travels[N]about the seven seas.[FIN]`
+widestring_08D094 `[TPL:16][TPL:E]I am Gem, a jewel[N]merchant that travels[N]about the seven seas.[FIN]`
 ;I am Gem, a jewel merchant that travels about the seven seas.
 
 widestring_08D0F1 `[DLG:2,11][SIZ:E,4][CLR]What's your business today?[N] Didn't mean to run into you[N] Deposit Red Jewels[N] See your list of goods`
 ;What business do we have today? / I didn't mean to see into you / I want to deposit Red Jewels / I want to see your list of goods
 
-widestring_08D139 `[CLD][TPL:16][TPL:F]Is that so. Anyway,[N]nice to meet you.[FIN]By the way, if you lift[N]up a jewel, it will fly[N]to my location, you know.[FIN]So something seems to be[N]hidden. I am a master of[N]disguise, after all. But[N]in other towns, maybe[WAI][N]you won't recognize me.[END]`
+widestring_08D139 `[CLD][TPL:16][TPL:D]Is that so?[N]Anyway, nice to meet you.[FIN][TPL:E]By the way, if you lift[N]a Red Jewel, it will fly[N]to my location, you know.[FIN][TPL:F]Sometimes that place may[N]seem hidden. I am a master[N]of disguise, after all.[N]In other towns, you might[WAI][N]not always recognize me.[END]`
 ;Is that so. Anyway, nice to meet you.
 ;By the way, if you lift up a jewel, it will fly to my location, you know.
 ;So something seems to be hidden. I am a master of disguise, after all. But in other towns, maybe you won't recognize me.
@@ -132,7 +132,7 @@ widestring_08D139 `[CLD][TPL:16][TPL:F]Is that so. Anyway,[N]nice to meet you.[F
 widestring_08D1E0 `[CLD][TPL:16][TPL:E]But you [LU1:82][LU1:98]any[N]Jewels. Let me give[N]you some.[END]`
 
 
-widestring_08D20F `[CLD][TPL:16][TPL:F]Hmm… These are truly good[N]jewels, aren't they?[N]Please, it is important[N]to take care of them.[FIN]With this, I think the[N]number of jewels should[N]have become [BCD:2,AB0][N]altogether.[FIN]`
+widestring_08D20F `[CLD][TPL:16][TPL:F]Hmm… These are truly good[N]jewels, aren't they?[N]Please, it is important[N]to take care of them.[FIN]With this, I think the[N]number of jewels should[N]have become [BCD:2,AB0] altogether.[FIN]`
 ;Hmm… These are truly good jewels, aren't they? Please, it is important to take care of them.
 ;With this, I think the number of jewels should have become 1 altogether.
 
@@ -144,20 +144,20 @@ widestring_08D267 `[CLD][TPL:16][TPL:F]I will give you the[N]commodity only when
 ------------------------
 ?INCLUDE 'sc01_guard'
 ------------------------
-widestring_0485B6 `[TPL:16][TPL:F]Hey, hey. Monsters have[N]been seen outside of[N]town recently.[FIN]Did the priest in the[N]church not warn you? You[N]shouldn't thoughtlessly[N]go outside the town.[END]`
+widestring_0485B6 `[TPL:16][TPL:E]Hey, hey! Monsters have[N]been seen outside of[N]town recently![FIN][TPL:F]Did the priest in the[N]church not warn you? You[N]shouldn't thoughtlessly[N]go outside the town.[END]`
 ;Hey, hey. Monsters have been seen outside of town recently.
 ;Did the priest in the church not warn you? You shouldn't thoughtlessly go outside the town.
 
 ------------------------
 ?INCLUDE 'sc01_astronomer'
 ------------------------
-widestring_0488FB `[TPL:16][TPL:E]You know, an astronomer[N]who is an acquaintance of[N]mine said something odd.[FIN]Something like,[N]"a star is approaching[N]our Earthˮ.[END]`
+widestring_0488FB `[TPL:16][TPL:E]You know, an acquaintance[N]of mine is an astronomer?[N]He said something strange…[FIN]"A star is approaching[N]our Earth," or something[N]like that…[END]`
 ;You know, an astronomer who is an acquaintance of mine said something strange. Something like, "a star is approaching our Earthˮ.
 
 ------------------------
 ?INCLUDE 'sc01_worried_woman'
 ------------------------
-widestring_04871E `[TPL:16][TPL:E]There have been a lot of[N]strange merchants around[N]recently so I'm worried.[FIN][TPL:F]I guess they're in that[N]business because of[N]everyone saying they're[N]worried about the[WAI][N]appearance of monsters…[END]`
+widestring_04871E `[TPL:16][TPL:E]I'm getting worried about[N]all of these strange[N]merchants we've been[WAI][N]seeing around here lately.[FIN]The stories of monsters[N]appearing outside of towns[N]must be good for business.[END]`
 ;There have been a lot of strange merchants around recently so I'm worried.
 ;I guess they're in that business because of everyone saying they're worried about the appearance of monsters…
 
@@ -177,7 +177,7 @@ widestring_04BD9B `[TPL:0][TPL:C]My friend Eric lives here.[FIN][TPL:F]This hous
 ------------------------
 ?INCLUDE 'sc01_salesman'
 ------------------------
-widestring_0482D7 `[TPL:16][TPL:F]Salesman: This old man[N]you're speaking to goes[N]around to everyone's[N]houses to sell weapons[WAI][N]for fighting with demons.[FIN]It's because the world's[N]become dangerous recently.[N]But I won't go selling[N]weapons to a child[WAI][N]like you.[END]`
+widestring_0482D7 `[TPL:16][TPL:E]Salesman: This old man[N]you're speaking to goes[N]around to every house[WAI][N]to sell weapons used[N]for fighting demons.[FIN]It's because the world's[N]become so much more[N]dangerous these days.[FIN]But I'm still not going to[N]go and sell weapons to a[N]child like you.[END]`
 ;Salesman: This old man you're speaking to goes around to everyone's houses to sell weapons for fighting with demons.
 ;It's because the world's become dangerous recently. But I won't go selling weapons to a child like you.
 
@@ -222,7 +222,7 @@ widestring_0487F1 `[JMP:&widestring_0487BF+M]`
 ------------------------
 ?INCLUDE 'sc01_sympathetic_woman'
 ------------------------
-widestring_04899E `[TPL:16][TPL:D]I can't help but pity[N]Morris.[FIN][TPL:F]I can't stand that even a[N]child has to see that kind[N]of marital quarrel every[N]day, you know?[END]`
+widestring_04899E `[TPL:16][TPL:D]I can't help but pity[N]Morris.[FIN][TPL:F]I can't stand it when[N]children have to see[N]their parents quarrel[N]every day, you know?[END]`
 ;I can't help but pity Morris.
 ;I can't stand that even a child has to see that kind of marital quarrel every day, you know?
 
@@ -247,7 +247,7 @@ widestring_048A48 `[TPL:0][TPL:E]Just as I opened the door[N]to Morris' house, a
 ------------------------
 ?INCLUDE 'sc03_lances_mother'
 ------------------------
-widestring_048F19 `[TPL:16][TPL:F]Rob's mother: It's already[N]been one year since my[N]husband went missing with[N]your father at the Tower[WAI][N]of Babel, hasn't it?[FIN]Somehow it still feels[N]like yesterday…[END]`
+widestring_048F19 `[TPL:16][TPL:F]Rob's Mother: It's already[N]been one year since my[N]husband went missing with[N]your father at the Tower[WAI][N]of Babel, hasn't it?[FIN]Somehow it still feels[N]like yesterday…[END]`
 ;Rob's mother: It's already been one year since my husband went missing with your father at the Tower of Babel, hasn't it?
 ;Somehow it still feels like yesterday…
 
@@ -261,27 +261,27 @@ widestring_048F9D `[TPL:16][TPL:F]You know, a little[N]thing about this this[N]w
 ------------------------
 ?INCLUDE 'sc04_eriks_father'
 ------------------------
-widestring_048FEF `[TPL:16][TPL:F]Eric's father: Everyone[N]seems to be jealous of[N]our big house…[FIN]But it's not a big deal.[N]It's just that our family[N]moved to this town a bit[N]before everyone.[END]`
+widestring_048FEF `[TPL:16][TPL:F]Eric's Father: Everyone[N]seems to be jealous of[N]our big house…[FIN]But it's not a big deal.[N]It's just that our family[N]moved to this town a bit[N]before everyone.[END]`
 ;Eric's father: Everyone seems to be jealous of our big house…
 ;But it's not a big deal. It's just that our family moved to this town a bit before everyone.
 
 ------------------------
 ?INCLUDE 'sc04_eriks_mother'
 ------------------------
-widestring_04907B `[TPL:16][TPL:F]Eric's mother: Don't[N]worry, my back isn't on[N]fire, haha. This what you[N]call "moxibustionˮ.[FIN]With a big house, even[N]when you're just cleaning,[N]you can't help hurting[N]your shoulder…[END]`
+widestring_04907B `[TPL:16][TPL:F]Eric's Mother: Don't[N]worry, my back isn't on[N]fire, haha. This what you[N]call "moxibustionˮ.[FIN]With a big house, even[N]when you're just cleaning,[N]you can't help hurting[N]your shoulder…[END]`
 ;Eric's mother: Don't worry, my back isn't on fire, haha. This what you call "moxibustionˮ.
 ;With a big house, even when you're just cleaning, you can't help hurting your shoulder…
 
 ------------------------
 ?INCLUDE 'sc05_seths_father'
 ------------------------
-widestring_049193 `[TPL:16][TPL:B]Morris's father: Hmph.[N]What's wrong with[N]drinking alcohol with[N]the money I earned!?[END]`
+widestring_049193 `[TPL:16][TPL:B]Morris' Father: Hmph.[N]What's wrong with[N]drinking alcohol with[N]the money I earned!?[END]`
 ;Morris's father: Hmph. What's wrong with drinking alcohol with the money I earned!?
 
 ------------------------
 ?INCLUDE 'sc05_seths_mother'
 ------------------------
-widestring_049110 `[TPL:16][TPL:9]Morris's mother: It's not[N]a joke…! That man…![FIN][TPL:B]Because of my son, Morris,[N]I'm tolerating it. But if[N]not for that, I'd have[N]divorced him long ago…![END]`
+widestring_049110 `[TPL:16][TPL:9]Morris' Mother: It's not[N]a joke…! That man…![FIN][TPL:B]Because of my son, Morris,[N]I'm tolerating it. But if[N]not for that, I'd have[N]divorced him long ago…![END]`
 ;Morris's mother: It's not a joke…! That man…!
 ;Because of my son, Morris, I'm tolerating it. But if not for that, I'd have divorced him long ago…!
 
