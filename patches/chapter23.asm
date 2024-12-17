ffc95174ba297bd214a72b3e65cf57cf68ace315
Bryan Moulton <azarem@gmail.com>
2024-12-17 03:03:54 -0500

    Chapter 23

--- patches/chapter23.asm
+++ patches/chapter23.asm
@@ -1,37 +1,30 @@
-
+;CHAPTER 23
 
 
 -------------------------------------
 ?INCLUDE 'av70_hidden_passage'
 -------------------------------------
-widestring_06D5ED `[TPL:9][TPL:0]The [LU2:C5]blows through[N]a crack in the wall.[FIN]I [LU1:8F]a hidden pass![PAL:0][END]`
-;It seems like wind is blowing in from a crack in the wall.
-;Found a hidden passage!
+widestring_06D5ED `[TPL:0][TPL:A]It seems like wind is[N]blowing in from a crack in[N]the wall.[FIN]Found a hidden passage![END]`
 
 -------------------------------------
 ?INCLUDE 'av70_crawlspace'
 -------------------------------------
-widestring_06D66C `[TPL:A][TPL:0]The entrance is[N]too small![PAL:0][END]`
-;The entrance is small; you can't pass though!
+widestring_06D66C `[TPL:0][TPL:9]The entrance is small;[N]you can't pass though![END]`
 
 -------------------------------------
 ?INCLUDE 'av74_kara'
 -------------------------------------
-widestring_06D2FF `[TPL:A][TPL:0][SFX:10]Will: Kara's picture. [N]She is contained [N][LU1:A2]it....[PAL:0][END]`
-;Tim: A picture of Karen. Has she been absorbed into this…?
+widestring_06D2FF `[TPL:0][TPL:A][SFX:10][NAM:0]: A picture of [NAM:1].[N]Has she been absorbed[N]into this…?[END]`
 
 -------------------------------------
 ?INCLUDE 'av74_ishtar'
 -------------------------------------
-widestring_06CEF2 `[TPL:A][TPL:3]Ishtar: I [LU1:F9]if you're[N][LU2:73]to get Kara. [FIN]Go [LU1:A1][LU1:D6]room.[FIN]If you solve all the[N]riddles, [LU1:1F][LU1:97]back[N]the girl. [END][PAL:0][END]`
-;Ishtar: You came to take back that girl, haven't you? She was named Karen or something.
-;You should go to the rooms ahead of here in that case. If you can solve all of the mysteries, I'll restore that girl to normal.
+widestring_06CEF2 `[TPL:3][TPL:B]Ishtar: You came to take[N]back that girl, haven't[N]you? She was named [NAM:1][N]or something.[FIN]You should go to the rooms[N]ahead of here in that[N]case.[FIN]If you can solve all of[N]the mysteries, I'll[N]restore that girl to[N]normal.[END]`
 
 -------------------------------------
 ?INCLUDE 'av75_apprentice'
 -------------------------------------
-widestring_06DC97 `[TPL:B][LU2:17]apprentice:[N][LU1:61]you solve the[N]puzzle of the room[N]you may pass.[END]`
-;Ishtar's apprentice: You can pass when you solve the mystery of the room before you.
+widestring_06DC97 `[TPL:1D][TPL:B]Ishtar's apprentice: You[N]can pass when you solve[N]the mystery of the room[N]before you.[END]`
 
 -------------------------------------
 ?INCLUDE 'av75_voice_doors'
@@ -42,150 +35,70 @@ widestring_06D83E `[TPL:A][LU2:16][LU1:EE]resounds.[FIN][LU1:D]hurry.[N]Open the
 -------------------------------------
 ?INCLUDE 'av75_voice_rooms'
 -------------------------------------
-widestring_06DA14 `[TPL:9][LU2:16][LU1:EE]resounds.[FIN]Learn well the condition[N]of [LU1:D7]room.[FIN][LU1:61]you've learned it, [N][LU1:AC]the room. [END]`
-;Ishtar's voice echoes.
-;Carefully memorize the state of that room.
-;When you think you have it completely memorized you may leave the room.
+widestring_06DA14 `[TPL:8]Ishtar's voice echoes.[FIN][TPL:3][TPL:9]Carefully memorize the[N]state of that room.[FIN][TPL:A]When you think you have it[N]completely memorized you[N]may leave the room.[END]`
 
-widestring_06DA6B `[TPL:9][LU2:16][LU1:EE]resounds.[FIN][LU2:37]how [LU1:9F]different[N][LU1:8E]the room before.[END]`
-;Ishtar's voice echoes.
-;Indicate the place that differs from the previous room before you.
+widestring_06DA6B `[TPL:8]Ishtar's voice echoes.[FIN][TPL:3][TPL:A]Indicate the place that[N]differs from the previous[N]room before you.[END]`
 
-widestring_06DAF0 `[TPL:A][LU2:2C]answer![N]The jar has[N][LU1:7F]color![FIN]Good.[N]Go on to the [LU1:BC]room.[END]`
-;That's correct!! You've noticed that the jar's colour was different, haven't you.
-;Very good. You may advance to the next room.
+widestring_06DAF0 `[TPL:3][TPL:8]That's correct!![FIN][TPL:A]You've noticed that the[N]jar's colour was[N]different, haven't you.[FIN]Very good. You may[N]advance to the next room.[END]`
 
-widestring_06D87B `[TPL:A][LU2:16][LU1:EE]resounds.[FIN]That [LU2:58]is already[N]open. There's no[N][LU1:BA]to open it.[END]`
-;That door was already been opened. It isn't necessary to open it again.
+widestring_06D87B `[TPL:8]Ishtar's voice echoes.[FIN][TPL:3][TPL:A]That door has already been[N]opened. It isn't necessary[N]to open it again.[END]`
 
-;Carefully memorize the state of that room.
-;When you think you have it completely memorized you may leave the room.
+widestring_06DB2C `[TPL:3][TPL:8]That's correct![FIN][TPL:9]Very good. You may advance[N]to the next room.[END]`
 
-;Ishtar's voice echoes.
-;Indicate the place that differs from the previous room.
+widestring_06DB4F `[TPL:3][TPL:8]That's correct!![FIN][TPL:B]You've realized that the[N]treasure chest's contents[N]are different, haven't[N]you.[FIN]Very good.[N]You may advance to the[N]next room.[END]`
 
-widestring_06DB2C `[TPL:A][LU2:2C]answer![FIN][LU1:18]Go on[N]to the [LU1:BC]room.[END]`
-;That's correct!
-;Very good. You may advance to the next room.
+widestring_06DB9F `[TPL:3][TPL:8]That's correct!![FIN][TPL:A]Your hair was fluttering[N]because the wind was[N]blowing, wasn't it.[FIN]You have passed my tests[N]splendidly. Alright, you[N]may return.[END]`
 
-;Ishtar's voice echoes.
-;Carefully memorize the state of that room.
-;When you think you have it completely memorized you may leave the room.
+widestring_06CF5E `[TPL:3][TPL:9]Ishtar: I had been[N]waiting for you to come.[FIN][TPL:B]You should sprinkle this[N]magic powder over the[N]picture of that girl, then[N]give it a heartfelt kiss.[FIN]Something should happen if[N]you truly feel for that[N]girl. Well, you'll[N]probably understand[WAI][N]eventually.[FIN][CLD][TPL:3][TPL:9]I painted my own[N]self-portrait.[FIN]I will soon be assimilated[N]into the picture…[FIN]That girl…[N]Protect her carefully…[END]`
 
-;Found a Medicinal Herb!
-
-;Ishtar's voice echoes.
-;Indicate the place that differs from the previous room.
-
-widestring_06DB4F `[TPL:A][LU2:2C]answer![N]How [LU1:98]the contents of[N]the Jewel Box changed?[FIN][LU1:18]Go on[N]to the [LU1:BC]room.[END]`
-;That's correct!! You've realized that the treasure chest's contents are different, haven't you.
-;Very good. You may advance to the next room.
-
-;Found a Red Jewel!
-
-;Ishtar's voice echoes.
-;Carefully memorize the state of that room.
-;When you think you have it completely memorized you may leave the room.
-
-;Ishtar's voice echoes.
-;Indicate the place that differs from the previous the room.
-
-widestring_06DB9F `[TPL:A][LU2:2C]answer![N]The [LU2:C5]blew your[N]hair around.[FIN]You [LU1:98]passed my[N]test well.[N]You may return.[END]`
-;That's correct!! Your hair was fluttering because the wind was blowing, wasn't it.
-;You have passed my tests splendidly. Alright, you may return.
-
-widestring_06CF5E `[TPL:A][TPL:3]Ishtar:[N]I [LU1:98]been[N][LU1:FD]for you.[FIN]Sprinkle magic powder[N]on the painting, and[N][LU1:97]it a kiss.[FIN]If you [LU2:57][LU1:6B]her [N]deeply, [LU1:C8][LU1:EF][N]happen. You'll see. [FIN]I painted a[N]self-portrait.[FIN][LU2:38]I [LU1:EF]become[N]the painting...[FIN]You [LU1:B2][LU1:E7]care[N]of her...[PAL:0][END]`
-;Ishtar: I had been waiting for you to come.
-;You should sprinkle this magic powder over the picture of that girl, then give it a heartfelt kiss.
-;Something should happen if you truly feel for that girl. Well, you'll probably understand eventually.
-;I painted my own self-portrait. I will soon be assimilated into the picture...
-;That girl... Protect her carefully...
-
-
-widestring_06D039 `[TPL:A][TPL:3].............[PAL:0][END]`
-;…
+widestring_06D039 `[TPL:3][TPL:8]…[END]`
 
 ;Found the magic powder!
 
 ;Magic powder / Magic powder which releases people who have been absorbed into a picture.
 
-widestring_06D278 `[TPL:A][TPL:0][SFX:10]Will: [N]If I [LU1:82]spread [N]magic dust...[PAL:0][END]`
-;Tim: I need to sprinkle the magic powder to start with...
+widestring_06D278 `[TPL:0][TPL:A][NAM:0]: I need to sprinkle[N]the magic powder to start[N]with…[END]`
 
 ;Tried using the magic powder.
 
 ;Sprinkled the magic powder on the picture of Karen!
 
-widestring_06D2A4 `[TPL:E][TPL:0][SFX:10]Will: [N][LU1:26]- [LU2:8E]return to [N][LU1:FE][LU1:C0]form.... [FIN][SFX:0]Will gently kisses the [N]picture of Kara...[PAL:0][END]`
-;Tim: I'm begging you. Karen... Please return to your original form...
-;Tim placed a heartfelt kiss on Karen's picture...
+widestring_06D2A4 `[TPL:0][TPL:E][NAM:0]: I'm begging you.[N][NAM:1]… Please return to[N]your original form…[FIN][CLD][TPL:D][NAM:0] placed a heartfelt[N]kiss on [NAM:1]'s picture…[END]`
 
-widestring_06D339 `[TPL:A][TPL:1][SFX:1B][LU1:25][N]Will....Sorry [N]for [LU1:77]so selfish.[FIN]`
-;Karen: Tim... I'm sorry for being selfish...
+widestring_06D339 `[TPL:1][TPL:9][NAM:1]: [NAM:0]… I'm sorry for[N]being selfish…[FIN]`
 
-widestring_06D364 `[TPL:A][CLR][TPL:0][SFX:10]Will: Kara!! [N]You [LU2:84]me so mad!! [FIN]You are not the [LU2:89][N]person on [LU1:D6]journey!  [FIN][TPL:1][SFX:1B][LU1:25][N]............[PAL:0][END]`
-;Tim: Agh, you've been terrible...! You've been incredibly bad...!!
-;Are you so irresponsible that you're willing to cause trouble for other people...!?
-;Karen: …
+widestring_06D364 `[TPL:0][TPL:A][NAM:0]: Agh, you've been[N]terrible…! You've been[N]incredibly bad…!![FIN][TPL:B]Are you so irresponsible[N]that you're willing to[N]cause trouble for other[N]people…!?[FIN][CLD][TPL:1][TPL:8][NAM:1]: …[END]`
 
-widestring_06D3CC `[TPL:A][TPL:1][SFX:1B][LU1:25][N]Whaaaaaah! [N]Sob....Sniff.... [FIN]I...Sob... I don't[N][LU1:A4][LU1:F7]I'm doing[N]myself...Sob...[FIN][LU1:61]I was in the[N]castle....I [LU1:7A]have[N][LU1:6C]I wanted...[FIN]But I was a completely[N][LU1:86]person[N][LU1:74][LU1:D6]trip...[FIN][TPL:0][SFX:10]Will: Naturally! [LU1:1D]a [N]mistake to [LU1:DA]you [N]can control everything!![FIN][TPL:1][SFX:1B][LU1:25]No! [FIN][LU1:61]I'm far away, I [LU1:91][N]close to it.[FIN][LU1:61]I'm close,[N]I [LU1:91]far away. [N]I realize [LU1:D7]now... [FIN][LU1:1D]all [LU2:94]if you[N][LU1:82]understand...[FIN]I [LU1:EF][LU1:BB]forget[N][LU1:F7][LU1:9D]today.[PAL:0][END]`
-;Karen: Waaaaaaaaaaah! *Sob*... *Sniff*...
-;I... *Sob* I don't even know.. why I'm doing it myself... *Sniff*...
-;The thing is, when I was in the castle... I got everything that I wanted...
-;But since I've been on this journey, it's been totally different...
-;Tim: Isn't it obvious...!? When you're only thinking of yourself, you're in trouble when things get serious…!!
-;Karen: That's not it...!
-;Even when you're far away, I feel that you're close, and even when you're close to me I feel that you're far away...
-;If you don't understand, that's fine...
-;But I don't think I'll forget what happened today for the rest of my life...
+widestring_06D3CC `[TPL:1][TPL:9][NAM:1]: Waaaaaaaaaaah![N]*Sob*… *Sniff*…[FIN][TPL:A]I… *Sob* I don't even[N]know… why I'm doing it[N]myself… *Sniff*…[FIN]The thing is, when I was[N]in the castle… I got[N]everything that I wanted…[FIN]But since I've been on[N]this journey, it's been[N]totally different…[FIN][CLD][TPL:0][TPL:8][NAM:0]: Isn't it obvious…!?[FIN][TPL:B]When you're only thinking[N]of yourself, you're in[N]trouble when things get[N]serious…!![FIN][CLD][TPL:1][TPL:8][NAM:1]: That's not it…![FIN][TPL:9]Even when you're far away,[N]I feel that you're close.[FIN][TPL:A]And even when you're close[N]to me I feel that you're[N]far away…[FIN]If you don't understand,[N]that's fine…[FIN][TPL:B]But I don't think I'll[N]forget what happened[N]today for the rest of my[N]life…[END]`
 
 -------------------------------------
 ?INCLUDE 'av6A_erik'
 -------------------------------------
-widestring_06C568 `[TPL:A][TPL:6][SFX:1A][LU1:36][N]Kara! I was worried! [FIN][TPL:2][SFX:19]Lilly:[N]Why are you always[N][LU2:97][LU1:6D]alone?![FIN][LU2:7]you [LU1:DA]about[N]the [LU2:9C]of us?[FIN][TPL:1][SFX:1B][LU1:25][N]Will already yelled [N]at me [LU1:6B]that. [FIN]My apologies to [N]everyone! [FIN][TPL:6][SFX:1A][LU1:36]She understands [N]now. We [LU1:CA][N]forgive her. [FIN]I [LU1:DA]the Floating[N][LU1:8]is [LU1:6B]three days[N]south of here.[FIN]I [LU1:DA]we [LU1:CA]go[N][LU1:D9][LU2:94]away. Tell[N]me [LU1:F6][LU1:FF]ready.[PAL:0][END]`
-;Neil: Karen! I was worried...!!
-;Lillie: Why do you always run around by yourself...!?
-;Don't you have any concern for anyone else...!?
-;Karen: If you're talking about that, Tim already scolded me.
-;Because of that, I sincerely apologize to everyone...! *Bow*…
-;Neil: Well, it seems that even the perpetrator understands the matter, so I can tolerate it.
-;Anyway, it seems that if we proceed about three days to the south, you get to the Floating City.
-;I think we'll try to leave right away. Talk to me when you're prepared to leave.
+widestring_06C568 `[TPL:6][TPL:9]Neil: [NAM:1]![N]I was worried…!![FIN][TPL:2][TPL:9][NAM:2]: Why do you always[N]run around by yourself…!?[FIN]Don't you have any concern[N]for anyone else…!?[FIN][TPL:1][TPL:A][NAM:1]: If you're talking[N]about that, [NAM:0] already[N]scolded me.[FIN]Because of that, I[N]sincerely apologize to[N]everyone…! *Bow*…[FIN][TPL:6][TPL:B]Neil: Well, it seems that[N]even the perpetrator[N]understands the matter,[N]so I can tolerate it.[FIN]Anyway, it seems that if[N]we proceed about three[N]days to the south, you get[N]to the Floating City.[FIN]I think we'll try to[N]leave right away. Talk to[N]me when you're prepared[N]to leave.[END]`
 
-widestring_06C6A0 `[TPL:A][TPL:3]Erik: I [LU1:DA]I saw [N]a Red Jewel [N]in the Angel Village.[PAL:0][END]`
-;Eric: Ah, speaking of that. In the fallen angels' village, I noticed something that looked like a Red Jewel.
+widestring_06C6A0 `[TPL:3][TPL:9][NAM:3]: Ah, speaking of[N]that…[FIN][TPL:B]In the fallen angels'[N]village, I noticed[N]something that looked[N]like a Red Jewel.[END]`
 
 -------------------------------------
 ?INCLUDE 'av6A_neil'
 -------------------------------------
-widestring_06C10D `[TPL:A][TPL:6][LU1:36]Can we go now? [N] Yes [N] [LU2:42]a minute `
-;Neil: Is it okay to go? / Yes / Wait a bit
+widestring_06C10D `[TPL:6][TPL:A]Neil: Is it okay to go?[N][PAL:0] Yes[N] Wait a bit`
 
-widestring_06C136 `[CLR][LU1:36][N]There's no hurry. [N]Take [LU1:FE]time.[PAL:0][END]`
-;Neil: Don't rush things. Take your time finishing your business.
+widestring_06C136 `[CLR][PAL:4]Neil: Don't rush things.[N]Take your time finishing[N]your business.[END]`
 
 -------------------------------------
 ?INCLUDE 'av6A_lily'
 -------------------------------------
-widestring_06C2F8 `[TPL:A][TPL:2][LU1:2A][N][LU1:26][LU1:AA]a  [N][LU1:A7]strange... [FIN]Has [LU1:C8][LU1:9D][N]to Kara? My intuition [N]is usually good.[PAL:0][END]`
-;Lillie: Karen seems to be strange somehow, doesn't she…?
-;I have a sense about these things; was something wrong with Karen?
+widestring_06C2F8 `[TPL:2][TPL:A][NAM:2]: [NAM:1] seems to be[N]strange somehow, doesn't[N]she…?[FIN]I have a sense about these[N]things; was something[N]wrong with [NAM:1]?[END]`
 
 -------------------------------------
 ?INCLUDE 'av6A_lance'
 -------------------------------------
-widestring_06BFBE `[TPL:B][TPL:4]Lance: [N][LU1:2C][LU1:EF][LU1:98]her [N][LU2:4C][LU2:BF]we're in [N]the Floating City...[PAL:0][END]`
-;Rob: I suppose that Lillie's birthday will come when we're in the Floatin' City…
+widestring_06BFBE `[TPL:4][TPL:B][NAM:4]: I suppose that[N][NAM:2]'s birthday will[N]come when we're in the[N]Floatin' City…[END]`
 
 -------------------------------------
 ?INCLUDE 'av6A_kara'
 -------------------------------------
-widestring_06C417 `[TPL:A][TPL:1][LU1:25]In the Floating [N]City, [LU1:B6][LU2:77][N]are built on rafts. [FIN]Kind of romantic.[N]I [LU1:A5]it.[PAL:0][END]`
-;Karen: You know, they say that there are a lot of houses built on rafts in the place that they call the Floating City.
-;It's kind of romantic, isn't it? I think it's fun…
+widestring_06C417 `[TPL:1][TPL:B][NAM:1]: You know, they say[N]that there are a lot of[N]houses built on rafts in[N]the place that they call[WAI][N]the Floating City.[FIN]It's kind of romantic,[N]isn't it? I think it's[N]fun…[END]`
 
-widestring_06C15C `[CLR][LU1:36][N]I [LU1:DA][LU1:9F][LU1:ED]hot [N]in the Floating City. [FIN]Everyone be careful not[N]to get heat stroke.[PAL:0][END]`
-;Neil: It seems that the Floating CIty is a considerably hot place.
-;Everyone mustn't get heatstroke, so take care.
-
+widestring_06C15C `[CLR][PAL:4]Neil: It seems that the[N]Floating City is a[N]considerably hot place.[FIN]Everyone mustn't get[N]heatstroke, so take care.[END]`
 
