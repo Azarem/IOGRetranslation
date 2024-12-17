cf62dcc55e59e71ccccfd0c63d8047c29fc69193
Bryan Moulton <azarem@gmail.com>
2024-12-17 15:50:23 -0500

    Chapter 25

--- patches/chapter25.asm
+++ patches/chapter25.asm
@@ -8,16 +8,16 @@
 -------------------------------------
 ?INCLUDE 'gw82_trail_intro'
 -------------------------------------
-widestring_07B550 `[TPL:9][TPL:0]I [LU2:63]Lance's trail [N]to the [LU1:16]Wall. [FIN]A corridor stretched[N]to the [LU1:85]horizon.[END]`
+widestring_07B550 `[TPL:0][TPL:9]Rob's tracks led me to[N]the Great Wall of China.[FIN][TPL:A]The passageway continued[N]without end beyond the[N]horizon…[END]`
 ;Rob's tracks led me to the Great Wall of China.
-;The passageway continued without end beyond the horizon...
+;The passageway continued without end beyond the horizon…
 
 -------------------------------------
 ?INCLUDE 'gw82_necklace_stone'
 -------------------------------------
-widestring_07B611 `[TPL:A][TPL:0]A small [LU2:9D]falls.[FIN]Ha! This is part of the [N]necklace Lance [LU1:B3][N]for Lilly! [FIN]I picked up the stones.[PAL:0][END]`
+widestring_07B611 `[TPL:0][TPL:9]A pretty pebble has[N]fallen.[FIN][TPL:A]Aha! This is a part of the[N]necklace that Rob made for[N]Lillie…[FIN][CLD][TPL:9]You picked up the[N]necklace's stone for him.[END]`
 ;A pretty pebble has fallen.
-;Aha! This is a part of the necklace that Rob made for Lillie...
+;Aha! This is a part of the necklace that Rob made for Lillie…
 ;You picked up the necklace's stone for him.
 
 ;Necklace's stones / A pendant that Rob made for Lillie.
@@ -27,27 +27,27 @@ widestring_07B611 `[TPL:A][TPL:0]A small [LU2:9D]falls.[FIN]Ha! This is part of 
 -------------------------------------
 ?INCLUDE 'gw83_lily'
 -------------------------------------
-widestring_07B6FF `[TPL:C][TPL:2]Wait![END]`
+widestring_07B6FF `[TPL:2][TPL:C]Wait![END]`
 ;Wait!!
 
-widestring_07B709 `[TPL:D][TPL:2][LU1:2A][N]Are you [LU1:AB]for Lance?[FIN][LU1:1F]go [LU1:F0]you![END]`
+widestring_07B709 `[TPL:2][TPL:D][NAM:2]: You're going to[N]search for Rob, right?[FIN]I'm going too…!![END]`
 ;Lillie: You're going to search for Rob, right?
-;I'm going too...!!
+;I'm going too…!!
 
-widestring_07B731 `[TPL:E][TPL:2][LU1:2A]Ha ha. [LU1:1D][LU1:70][N]a [LU1:A6][LU1:DF][LU2:A4]I [N]borrowed Will's pocket. [FIN][LU1:63]let's go.[END]`
+widestring_07B731 `[TPL:2][TPL:F][NAM:2]: Eheheh. It's been[N]a long time since I've[N]borrowed your pocket like[N]this, huh?[FIN]Alright, let's go…[END]`
 ;Lillie: Eheheh. It's been a long time since I've borrowed your pocket like this, huh?
 ;Alright, let's go…
 
 -------------------------------------
 ?INCLUDE 'sE6_gaia'
 -------------------------------------
-widestring_08E428 `[PRT:@widestring_08DE96]Will's [LU1:C2]is the [N][LU2:31]Dash. Use [LU1:D6]to [N]climb hills and jump. [FIN]There are [LU1:B6]hills at[N]the [LU1:16]Wall of China.[N]Try everything.[FIN][RET]`
+widestring_08E428 `[PRT:@widestring_08DE96][NAM:0]'s power Spin Dash can[N]jump to ascend slopes that[N]are too sleep to climb.[FIN]There are many hills here[N]in the Great Wall of[N]China. Try attempting it[N]in various places.[FIN][RET]`
 ;Tim's power Spin Dash can jump to ascend slopes that are too sleep to climb.
 ;There are many hills here in the Great Wall of China. Try attempting it in various places.
 
 ;It seems that the Power of Darkness Spin Dash become usable!
 
-widestring_08ECEA `[LU1:38]young Will can use [N]the [LU2:31]Dash. [FIN][LU2:31][LU1:FE][LU1:72]to[N]send [LU1:8C]flying,[N]and use the recoil[N]to climb hills.[FIN]Use the Attack and LR [N][LU2:3]for power. `
+widestring_08ECEA `Spin Dash is a power that[N]only [NAM:0], the boy, can[N]use.[FIN]Thine own high-speed[N]rotation can send enemies[N]flying. Thou can run up[N]hills that use recoil.[FIN]Thou shouldst alternate[N]between L and R while[N]holding the attack button…`
 ;Spin Dash is a power that only Tim, the boy, can use.
 ;Thine own high-speed rotation can send enemies flying. Thou can run up hills that use recoil.
 ;Thou shouldst alternate between L and R while holding the attack button…
@@ -61,53 +61,54 @@ widestring_08ECEA `[LU1:38]young Will can use [N]the [LU2:31]Dash. [FIN][LU2:31]
 -------------------------------------
 ?INCLUDE 'gw8A_sand_fanger'
 -------------------------------------
-widestring_0B80B5 `[DEF][TPL:0][LU1:67]defeated the [N]Sand Fanger!  [N]Look! A Mystic Statue![PAL:0][END]`
+widestring_0B80B5 `[TPL:F]After you defeated Sand[N]Fanger, you found a[N]Mystery Doll from its[N]corpse!![END]`
 ;After you defeated Sand Fanger, you found a Mystery Doll from its corpse!!
 
 -------------------------------------
 ?INCLUDE 'gw8B_lance_lily'
 -------------------------------------
-widestring_07B824 `[TPL:A][TPL:2]Lilly:[N][LU1:69]crazy! I've[N][LU1:70]worried sick![FIN][LU1:62]if you'd [LU1:70][N]attacked! [FIN][TPL:4]Lance: [N][LU2:35]to [LU1:98][N]worried [LU2:C7][FIN]But I got [LU1:D0]medicine[N]to cure my father.[PAL:0][END]`
-;Lillie: Rob, you idiot…You had me worried...!!
-;What would you do if the demons killed you...!!
+widestring_07B824 `[TPL:2][TPL:9][NAM:2]: [NAM:4], you idiot…[N]You had me worried…!![FIN]What would you do if the[N]demons killed you…!![FIN][TPL:4][TPL:9][NAM:4]: It's my bad fer[N]worryin' everyone.[FIN]But I obtained medicine ta[N]cure my old man's illness.[END]`
+;Lillie: Rob, you idiot…You had me worried…!!
+;What would you do if the demons killed you…!!
 ;Rob: It's my bad fer worryin' everyone.
 ;But I obtained medicine ta cure my old man's illness.
 
-widestring_07BB89 `[TPL:A][TPL:2]Lilly:[N]Already? You're[N]selfish....[END]`
-;Lillie: Geez… He's selfish, after all...
+widestring_07BB89 `[TPL:2][TPL:9]Lillie: Geez…[N]He's selfish, after all…[END]`
+;Lillie: Geez… He's selfish, after all…
 
-widestring_07B8AD `[TPL:A][TPL:4]Lance: [N]Oh. That [LU2:9D]. . . [FIN][TPL:0]Will: If you follow the [N][LU2:9D]chips, the trail [N]leads here.[FIN][LU1:1F][LU1:97][LU1:E6]back[N]to [LU2:C7][WAI][CLD][PAU:3C][::][TPL:A][TPL:4][SFX:0][DLY:2]Lance whispers... [FIN]Lance: [N]Will...will you [LU1:E7][N][LU2:57]of [LU1:2C]for me? [END]`
-;Rob: Ah, those stones are...
-;Tim: We arrived here when we followed these pebbles. I'll return the stones to you. Here you go…
-;Rob whispered...
-;Rob: Tim... Won't ya leave me with Lillie fer just a little bit?
+widestring_07B8AD `[TPL:4][TPL:9]Rob: Ah, those stones[N]are…[FIN][TPL:0][TPL:9][NAM:0]: We arrived here when[N]we followed these pebbles.[FIN]I'll return the stones to[N]you. Here you go…[WAI][CLD][PAU:3C][::][TPL:4][TPL:8][SFX:0][DLY:2]Rob whispered…[FIN][TPL:A][NAM:4]: [NAM:0]…won't ya leave[N]me with [NAM:2] fer just a[N]little bit?[END]`
+;Rob: Ah, those stones are…
+;Tim: We arrived here when we followed these pebbles. 
+;I'll return the stones to you. Here you go…
+;Rob whispered…
+;Rob: Tim… Won't ya leave me with Lillie fer just a little bit?
 
-widestring_07B960 `[TPL:A][TPL:4][DLY:2]Lance:[N]I was saved thanks[N]to [LU1:E4]stones...[FIN]This was the necklace [N]I [LU1:B3]for [LU2:C7][END]`
-;Rob: I was saved with tha help o' these stones...
-;Tha truth is, I made this fer ya...
+widestring_07B960 `[TPL:4][TPL:9][DLY:2][NAM:4]: I was saved with[N]tha help o' these stones…[FIN]Tha truth is,[N]I made this fer ya…[END]`
+;Rob: I was saved with tha help o' these stones…
+;Tha truth is, I made this fer ya…
 
-widestring_07B9B1 `[TPL:A][TPL:4]Lance: There aren't [LU1:B6][N]necklace stones left. [N]Will you [LU1:E7]them? [FIN][SFX:0][TPL:6][DLY:2]Lance, fixing the [N]necklace, puts it [N][LU1:6D]her neck. [END]`
+widestring_07B9B1 `[TPL:4][TPL:B][NAM:4]: Tha stones in tha[N]necklace ended up bein'[N]awfully small, but won't[N]ya take it fer me?[FIN][CLD][TPL:A][SFX:0][DLY:2]Rob gently affixed the[N]necklace onto Lillie's[N]neck.[END]`
 ;Rob: Tha stones in tha necklace ended up bein' awfully small, but won't ya take it fer me?
 ;Rob gently affixed the necklace onto Lillie's neck..
 
-widestring_07BBB0 `[TPL:A][TPL:4][DLY:0]Lance: Aaah... [WAI][CLD][PAU:3C][TPL:A][TPL:2][DLY:2][LU1:2A][N]I [LU1:FA]run [LU1:D6]time. [FIN]This [LU1:9D]so [N]suddenly, I [LU1:83][LU1:A4][N][LU1:F7]to do... [FIN]I [LU1:82][LU1:F1]to show [N]my face now. [FIN]I'm crying[N][LU1:8E]happiness...[END]`
+widestring_07BBB0 `[TPL:4][TPL:8][DLY:0][NAM:4]: Ah…[WAI][CLD][PAU:40][TPL:2][TPL:9][DLY:2]Lillie: I won't run away[N]this time…[FIN][TPL:B]Last time, I wasn't[N]expecting what you did. I[N]was surprised and didn't[N]know what I should do…[FIN]But right now, I just[N]don't want you to be able[N]to see my face.[FIN]I'm so happy that my[N]tears are flooding out…[END]`
 ;Rob: Ah…
-;Lillie: I won't run away this time...
-;The last time I did this, I wasn't expecting what you did. I was surprised and didn't know what I should do...
+;Lillie: I won't run away this time…
+;The last time I did this, I wasn't expecting what you did. I was surprised and didn't know what I should do…
 ;But right now, I just don't want you to be able to see my face.
-;I'm so happy that my tears are flooding out...
+;I'm so happy that my tears are flooding out…
 
-widestring_07BC42 `[TPL:A][TPL:2][DLY:2][LU1:2A][LU1:20][LU2:44]felt[N][LU1:D9]was something[N][LU1:86][LU1:6B]you.[FIN]Now I [LU1:91]I know[N][LU1:F7]the difference is.[FIN]I [LU1:F1]to [LU1:97]you[N]an answer...[END]`
-;Lillie: Rob.. From the time when we first met, I felt something that was different...
-;But now I have a feel that I understand what that something is...
-;The reply I had for you back then... The reply that I wanted to give you...
+widestring_07BC42 `[TPL:2][TPL:B][DLY:2][NAM:2]: [NAM:4]…from the time[N]when we first met, I felt[N]something that was[N]different about you…[FIN][CLD][TPL:2][TPL:A][DLY:2]But now I have a feeling[N]that I understand what[N]that something is…[FIN]The reply I had for you[N]back then… The reply that[N]I wanted to give you…[END]`
+;Lillie: Rob. From the time when we first met, I felt something that was different…
+;But now I have a feel that I understand what that something is…
+;The reply I had for you back then… The reply that I wanted to give you…
 
-widestring_07BCAA `[TPL:A][TPL:2]Lilly:[N][DLY:3]I love you, too.[FIN][DLY:2]I [LU1:F1]to be with[N]you forever...[END]`
-;Lillie: I care about you, too...
+widestring_07BCAA `[TPL:2][TPL:9][NAM:2]:[N][DLY:3]I care about you, too…[FIN][TPL:A][DLY:2]It would be nice if we[N]could always be together,[N]wouldn't it…?[END]`
+;Lillie: I care about you, too…
 ;It would be nice if we could always be together, wouldn't it…?
 
-widestring_07BA2E `[TPL:A][TPL:4][DLY:0]Lance: Wow!! [LU1:20][LU1:BB][N][DLY:1]felt [LU1:D6]way before![FIN][LU1:1D][LU1:A5]a million [N]summer days! [FIN][TPL:2][DLY:1]Lilly:[N](Sob).[N]I [LU1:91]the [LU1:CD]way.[FIN][LU1:2B]go [LU1:73]to the[N][LU2:BD]I'm sure[N][LU1:8B]worried.[END]`
-;Rob: Yahoo...!! This is tha first time ever I've felt like this...!!
-;I'm almost a million times as happy as I am at the start of summer vacation...!
-;Lillie: Tee hee... I feel the saaame way.
+widestring_07BA2E `[TPL:4][TPL:A][DLY:0][NAM:4]: Yahoo!! This is tha[N]first time ever I've felt[N]like this…!![FIN]I'm almost a million times[N]as happy as I am at the[N]start of summer vacation…![FIN][TPL:2][TPL:A][DLY:1][NAM:2]: Tee hee…[N]I feel the saaame way.[FIN]Alright, let's return to[N]town. Everyone must be[N]worried.[END]`
+;Rob: Yahoo…!! This is tha first time ever I've felt like this…!!
+;I'm almost a million times as happy as I am at the start of summer vacation…!
+;Lillie: Tee hee… I feel the saaame way.
 ;Alright, let's return to town. Everyone must be worried.
