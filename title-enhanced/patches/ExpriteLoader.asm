9ecbaddfbeceb2f0b6f7bc5d3526e25b3ae00b0a
Bryan Moulton <azarem@gmail.com>
2025-03-15 16:22:30 -0400

    Minor fix

--- title-enhanced/patches/ExpriteLoader.asm
+++ title-enhanced/patches/ExpriteLoader.asm
@@ -48,9 +48,13 @@ func_03DFF8 {
     BEQ load_exit
     CMP #$FE
     BEQ load_exit
+    CMP #$F0
+    BEQ load_exit
     CMP #$90
     BEQ load_exit
     CMP #$8C
+    BEQ load_exit
+    CMP #$89
     BEQ load_exit
     CMP #$78
     BEQ load_01_asset
