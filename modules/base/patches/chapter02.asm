9e8e73091ab299f00b73ec1112dca2b9eb2267d2
ViewtifulSlayer <viewtifulslayer@gmail.com>
2025-11-25 05:23:45 -0500

    Update chapter02.asm
    Update chapter02.asm
    
    Update localization for Chapter 2 Seaside Cave prologue
    
    - Refine Chapter 2 dialogue translations (Rob, Morris, Erik)
    - Differentiate character voice (Rob's casual "yeah?" vs Erik's softer "right?")
    - Consolidate five senses explanation for text box economy
    
    All changes maintain faithfulness to original Japanese while improving English readability and natural phrasing.

--- modules/base/patches/chapter02.asm
+++ modules/base/patches/chapter02.asm
@@ -3,68 +3,66 @@
 ----------------------------
 ?INCLUDE 'sc02_entry'
 ----------------------------
-widestring_04BE87 `[TPL:0][TPL:A]It might be strange, but[N]we even call this coastal[N]cave our second home.[FIN][TPL:B]Usually, we meet here[N]after our lesson at church[N]is finished and chat until[N]it gets dark.[END]`
+widestring_04BE87 `[TPL:0][TPL:A]It wouldn't be wrong to[N]call this seaside cave our[N]second home.[FIN][TPL:B]Usually, we meet here[N]after our lessons at[N]church are finished and[N]chat until the sun sets.[END]`
 
-widestring_04BF35 `[TPL:4][TPL:A][NAM:4]: [N]What, [NAM:0]? [N]Going [LU2:72]already?[FIN][LU1:1D]not dinner [LU1:DF][N]yet. [LU1:2B]play a little[N][LU2:BF]longer.[END]`
-;TODO!!
+widestring_04BF35 `[TPL:4][TPL:9][NAM:4]: What the heck,[N][NAM:0]? Going home already?[FIN][TPL:A]Dinner time's still a ways[N]off, so let's stick around[N]and have fun a bit longer.[END]`
 
 ----------------------------
 ?INCLUDE 'sc02_lance'
 ----------------------------
-widestring_04B1CB `[TPL:4][TPL:9][NAM:4]: What's up, [NAM:0]?[N]Late, ain't ya…[FIN][TPL:A]I'm playin' a match o'[N]blackjack with [NAM:5][N]right now. Wait a sec.[END]`
+widestring_04B1CB `[TPL:4][TPL:9][NAM:4]: What gives, [NAM:0]?[N]You're pretty late, man.[FIN][TPL:B]Right now, I'm in the[N]middle of a match of[N]Blackjack with [NAM:5].[N]Wait a sec.[END]`
 
 ----------------------------
 ?INCLUDE 'sc02_seth'
 ----------------------------
-widestring_04B5B6 `[TPL:5][TPL:9][NAM:5]: Ahaha… It turns[N]out that I win again.[END]`
+widestring_04B5B6 `[TPL:5][TPL:9][NAM:5]: Ahaha! I'm sure to[N]win again.[END]`
 
 ----------------------------
 ?INCLUDE 'sc02_erik'
 ----------------------------
-widestring_04B9AF `[TPL:A][DLY:0]Suddenly, [NAM:3] burst in[N]with a strange expression[N]on his face![PAU:3C][CLD]`
+widestring_04B9AF `[TPL:0][TPL:A][DLY:0]Suddenly, [NAM:3] came flying[N]in with an urgent look[N]on his face![WAI][CLD]`
 
-widestring_04B9F2 `[TPL:3][BOR:2][TPL:9][NAM:3]: Huff, huff… News…![N]It's biiig news…![FIN][TPL:A]I heard that Edward[N]Castle's princess has[N]gone missing![FIN]Supposedly she seems to[N]be heading to this town![END]`
+widestring_04B9F2 `[TPL:3][BOR:2][TPL:9][NAM:3]: *huff* *huff*[N]News! It's BIG news![FIN][TPL:A]Edward Castle's princess[N]has gone missing! They say[N]she came to this town![END]`
 
-widestring_04BA55 `[TPL:4][TPL:8][NAM:4]: What?[FIN][TPL:A]With ya bargin' in here[N]like that, I thought[N]somethin' cooler happened.[FIN][TPL:B]Besides, that "princessˮ[N]is that selfish girl[N][NAM:1], yeah? Who cares[N]WHERE she is?[FIN][TPL:3][TPL:B][NAM:3]: Well, there's that,[N]but soldiers are going to[N]come to this town to[N]search for the princess.[FIN]Aren't Edward Castle's[N]soldiers cool? I just want[N]to look at them.[END]`
+widestring_04BA55 `[TPL:4][TPL:B][NAM:4]: Dude, so what? With[N]you barging in here like[N]that, I thought something[N]much cooler had happened.[FIN]Besides, that "princess"[N]is just that selfish girl[N][NAM:1], yeah? Who cares[N]about someone like that?[END][TPL:3][TPL:A][NAM:3]: That's true, but[N]soldiers will come around[N]here looking for her.[FIN][TPL:B]The soldiers from Edward[N]Castle just look so cool,[N]don't they? I really want[N]to see them.[END]`
 
-widestring_04B210 `[TPL:4][TPL:A][NAM:4]: Oy, [NAM:0], don't[N]hang around there. Go sit[N]next to [NAM:3].[END]`
+widestring_04B210 `[TPL:4][TPL:A][NAM:4]: Don't just stand[N]there, [NAM:0]. Go sit next[N]to [NAM:3].[END]`
 
-widestring_04B612 `[TPL:5][TPL:A][NAM:5]: I have no interest[N]in girls. Reading books[N]is more fun.[END]`
+widestring_04B612 `[TPL:5][TPL:A][NAM:5]: I'm not really[N]interested in girls.[N]Reading books is more fun.[END]`
 
-widestring_04BB59 `[TPL:3][TPL:B][NAM:3]: Guh. And I had been[N]thinking that maybe[N]everyone would be[N]surprised…[END]`
+widestring_04BB59 `[TPL:3][TPL:A][NAM:3]: Dang, I thought[N]everyone was gonna be so[N]amazed…[END]`
 
-widestring_04B652 `[TPL:5][TPL:A][NAM:5]: Alright, since[N]everyone's here, what[N]are we doing today?[FIN][TPL:3][TPL:A][NAM:3]: I think I want[N]to see [NAM:0]'s mysterious[N]powers.[FIN][TPL:B]Look, he showed it to us[N]the other day, didn't he?[N]The guy can move things[N]without using his hands.[FIN][TPL:4][TPL:B][NAM:4]: If I'm rememberin',[N]I think he moved tha stone[N]statue in this cavern's[N]nook.[FIN][CLD][TPL:4][TPL:9][NAM:0], show us one more[N]time.[END]`
+widestring_04B652 `[TPL:5][TPL:A][NAM:5]: Alright, since[N]everyone's here, what's[N]the plan for today?[FIN][CLD][TPL:3][TPL:9][NAM:3]: Hey, I wanna see[N]Tim's mysterious power.[FIN][TPL:B]Come on, didn't you show[N]us the other day? You can[N]move things without using[N]your hands.[END][TPL:4][TPL:A][NAM:4]: If I'm remembering[N]right, he moved the stone[N]statue in the cave's nook.[FIN][CLD][TPL:4][TPL:9]Show us one more time,[N][NAM:0].[END]`
 
-widestring_04BB89 `[TPL:3][TPL:A][NAM:3]: Recently, you moved[N]the stone statue without[N]being near it, right?[END]`
+widestring_04BB89 `[TPL:3][TPL:A][NAM:3]: You moved the statue[N]without going near it the[N]other day, right?[END]`
 
-widestring_04B23F `[TPL:A][TPL:4][NAM:4]: I think ya spun yer[N]flute like a baton and[N]pulled it towards ya.[END]`
+widestring_04B23F `[TPL:4][TPL:B][NAM:4]: If I recall, when[N]you spun your flute like a[N]baton, the statue was[N]pulled to you, yeah?[END]`
 
-widestring_04B72B `[TPL:A][TPL:5][NAM:5]: Face the stone[N]statue and press the[N]L or R button.[END]`
+widestring_04B72B `[TPL:5][TPL:A][NAM:5]: Face toward the[N]stone statue and press[N]the L or R button.[END]`
 
-widestring_04B75D `[TPL:4][BOR:2][TPL:9][NAM:4]: Whoa..! It[N]moooooved..!![PAU:28][END]`
+widestring_04B75D `[TPL:4][TPL:8][NAM:4]: Aah! It moooooved![PAU:28][END]`
 
-widestring_04B27E `[TPL:4][TPL:9][NAM:4]: Alright. This time,[N]do tha card reveal fer us.[FIN]From tha four cards I'm[N]placin' face down,[FIN][TPL:A]raise and show us one if[N]ya think it's tha ace o'[N]diamonds.[END]`
+widestring_04B27E `[TPL:4][TPL:9][NAM:4]: Alright. This time,[N]let's try the card trick.[FIN][TPL:B]I'll put four cards face[N]down. Pick the one you[N]think is the Ace of[N]Diamonds.[FIN][CLD]`
 
-widestring_04B2F0 `[TPL:4][TPL:A][NAM:4]: Come on. Find tha[N]one that ya think is tha[N]ace o' diamonds.[END]`
+widestring_04B2F0 `[TPL:4][TPL:A][NAM:4]: Go on. Choose the[N]one you think is the Ace[N]of Diamonds.[END]`
 
-widestring_04BBB6 `[TPL:3][TPL:A][NAM:3]: I think it would be[N]nice. If I could also use[N]a power like that…[END]`
+widestring_04BBB6 `[TPL:3][TPL:9][NAM:3]: If only I could do[N]that too…[END]`
 
-widestring_04B77C `[TPL:5][TPL:B][NAM:5]: No matter how[N]many times you see it,[N]it's still cool, isn't[N]it…?[FIN]However, I wonder why[N]things like the desk[N]don't move, and only the[N]stone statue moves…[END]`
+widestring_04B77C `[TPL:5][TPL:A][NAM:5]: No matter how many[N]times I see it, it's still[N]amazing.[FIN]But why can you move the[N]statue when you can't move[N]anything else…?[END]`
 
 ----------------------------
 ?INCLUDE 'sc02_card'
 ----------------------------
-widestring_04B00F `[TPL:A][NAM:0] raised a card.[WAI][N]Surely enough, it was[N]the ace of diamonds!![END]`
+widestring_04B00F `[TPL:16][BOR:0][TPL:8][NAM:0] picks up a card.[WAI][N][TPL:A]Beyond the shadow of a[N]doubt, it was the Ace of[N]Diamonds![END]`
 
-widestring_04B328 `[TPL:4][BOR:2][TPL:8][NAM:4]: Ah…! He got it…![END]`
+widestring_04B328 `[TPL:4][BOR:2][TPL:9][NAM:4]: Ahh. Right on the[N]money![END]`
 
-widestring_04BC16 `[TPL:3][TPL:9][NAM:3]: Phew. I'm[N]speechless…[FIN][TPL:B]Hey. [NAM:5]. You call[N]this sort of thing[N]"extrasensory perceptionˮ,[N]right?[END]`
+widestring_04BC16 `[TPL:3][TPL:9][NAM:3]: Phew. I'm[N]speechless…[FIN][CLD][TPL:3][TPL:8]Hey, [NAM:5].[FIN][TPL:A]This sort of thing is[N]called extrasensory[N]perception, right?[END]`
 
-widestring_04B7DE `[TPL:5][TPL:A][NAM:5]: You call it[N]"extrasensory perceptionˮ,[N]huh?[FIN]The word means a sense[N]that goes beyond the[N]human senses…[FIN]What we call the human[N]senses are the sense of[N]sight,[FIN][CLD][TPL:5][TPL:8]the sense of hearing,[FIN]the sense of taste,[FIN]the sense of smell,[FIN][TPL:A]and the sense of touch;[N]it's what we call these[N]five…[FIN][CLD][TPL:5][TPL:8]I'm not sure, but…[FIN][TPL:B]I'm thinking that what you[N]call extrasensory[N]perception is a sixth[N]sense.[END]`
+widestring_04B7DE `[TPL:5][TPL:A][NAM:5]: The meaning of[N]"extrasensory perception"[N]is…[FIN]just as the word says: the[N]ability to perceive things[N]beyond normal human sense.[FIN][TPL:B]We usually identify the[N]traditional five human[N]senses as sight, hearing,[N]taste, smell, and touch.[FIN][CLD][TPL:5][TPL:A]I'm thinking that[N]extrasensory perception[N]might be the sixth sense.[END]`
 
-widestring_04B344 `[TPL:4][TPL:B][NAM:4]: I don't understand[N]tha things that [NAM:5][N]tha scholar says all that[N]well.[END]`
+widestring_04B344 `[TPL:4][TPL:A][NAM:4]: I don't understand[N]the stuff Professor [NAM:5][N]says all that well.[END]`
 
-widestring_04B386 `[TPL:4][TPL:A][NAM:4]: [NAM:5]. Take out tha[N]cards; we'll have one[N]more match next.[END]`
+widestring_04B386 `[TPL:4][TPL:A][NAM:4]: Come on, [NAM:5].[N]While the cards are out,[N]let's play another game.[END]`
 
-widestring_04B3AD `[TPL:4][TPL:A][NAM:4]: I'll play another[N]match or so with [NAM:5][N]and then go home.[END]`
-
+widestring_04B3AD `[TPL:4][TPL:9][NAM:4]: One more game with[N][NAM:5] and I'm going home.[END]`
