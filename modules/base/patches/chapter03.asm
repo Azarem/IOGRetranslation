622210d8ecd07a500aa3cb9eb5713955595bfe1c
ViewtifulSlayer <viewtifulslayer@gmail.com>
2025-11-28 01:52:32 -0500

    Update chapter03.asm
    Update localization for Chapter 3 core dialogue
    
    - Refine Chapter 3 dialogue translations (NPCs, Tim, Karen, Laura, Bill, Soldiers)
    - Move Jeweler Gem includes to chapter01.asm
    - Implement [NAM:X] tags for Laura's dishes (dub/sub vs literal)
    - Restore cut/mistranslated joke relying on text color and wordplay
    - Add [CLD][PAU:0F] conversational pauses between speakers or topics to enhance flow and readability
    - Adjust text box formatting
    
    All changes maintain faithfulness to original Japanese while improving English readability and natural phrasing.

--- modules/base/patches/chapter03.asm
+++ modules/base/patches/chapter03.asm
@@ -3,96 +3,91 @@
 ----------------------------
 ?INCLUDE 'sc01_house_intro'
 ----------------------------
-widestring_04BE05 `[TPL:0][TPL:F]Around when I left the[N]coastal cave, the sky had[N]been completely painted[N]by twilight.[END]`
-
-------------------------
-?INCLUDE 'jeweler_gem'
-------------------------
-widestring_08D2D0 `[TPL:16][TPL:F]Whoops, the number of[N]jewels you've collected[N]have passed 3, haven't[N]they…![FIN][CLD][TPL:16][TPL:D]Going by the list, I'll[N]give you a Medicinal Herb.[FIN][CLD]`
-widestring_08D31A `[TPL:16][TPL:D]Received a Medicinal Herb[N]from the jewel merchant![FIN]`
+widestring_04BE05 `[TPL:0][TPL:E]Upon leaving the cave,[N]twilight had completely[N]painted the area.[END]`
 
 ------------------------
 ?INCLUDE 'sc06_hamlet'
 ------------------------
-widestring_04A311 `[TPL:0][TPL:9]The pig is destroying[N]the room…![FIN]However, why is there a[N]pig in my house…[END]`
-widestring_04A356 `[BOR:2][TPL:8]Squeal squeal[END]`
+widestring_04A311 `[TPL:0][TPL:A]There's a pig wrecking the[N]room![FIN]But why is there a pig in[N]my house…?[END]`
+widestring_04A356 `[BOR:2][TPL:8](oink oink)[END]`
 
 ------------------------
 ?INCLUDE 'sc06_kara'
 ------------------------
-widestring_049EB4 `[TPL:1][TPL:A][NAM:1]: Oh my, [NAM:7]. You[N]shouldn't bully people[N]you don't know.[FIN]Are you a child from this[N]house?[FIN][TPL:0][TPL:A][NAM:0]: Yeah… That's[N]right, but?[END]`
+widestring_049EB4 `[TPL:1][TPL:A][NAM:1]: Oh my, [NAM:7]. You[N]shouldn't bully people you[N]don't know.[FIN][CLD][TPL:1][TPL:9]Do you belong to this[N]household?[FIN][CLD][PAU:0F][TPL:0][TPL:8][NAM:0]: Uh-huh. So what?[END]`
 
-widestring_049F09 `[TPL:1][TPL:9][NAM:1]: The way you dress[N]looks a little drab, huh?[FIN][TPL:0][TPL:9][NAM:0]: Well, my bad![FIN][TPL:1][TPL:A][NAM:1]: Where is your[N]father? Your mother?[N]They're not here, huh?[END]`
+widestring_049F09 `[TPL:1][TPL:9][NAM:1]: Your clothes are a [N]bit drab, aren't they?[FIN][CLD][PAU:0F][TPL:0][TPL:8][NAM:0]: Well, excuse me![FIN][CLD][PAU:0F][TPL:1][TPL:A][NAM:1]: And your father?[N]Your mother? Are they not[N]around?[END]`
 
-widestring_049F71 `[TPL:1][TPL:D][NAM:1]: Are these your[N]parents in this picture?[FIN][TPL:0][TPL:D][NAM:0]: That's right. Dad[N]went on an expedition…[END]`
+widestring_049F71 `[TPL:1][TPL:D][NAM:1]: Are they the two in[N]this picture?[FIN][CLD][PAU:0F][TPL:0][TPL:D][NAM:0]: That's right. My dad[N]went on an expedition and…[END]`
 
-widestring_049FBC `[TPL:1][TPL:A][NAM:1]: I know. it was[N][NAM:9]'s expedition team,[N]right?[FIN][CLD][TPL:1][TPL:9]There was an accident,[N]huh.[FIN][TPL:0][TPL:9][NAM:0]: He's definitely[N]coming home someday.[FIN][TPL:1][TPL:9][NAM:1]: Are you angry?[N]…That's wrong, huh.[FIN]I must have made you sad…[N]I'm sorry…[END]`
+widestring_049FBC `[TPL:1][TPL:B][NAM:1]: I know. It was the[N][NAM:9] expedition party,[N]and there was an accident,[N]right?[FIN][CLD][PAU:0F][TPL:0][TPL:9][NAM:0]: He's definitely[N]coming home someday.[FIN][CLD][PAU:0F][TPL:1][TPL:9][NAM:1]: Are you angry…?[N]No, that's not it.[FIN][TPL:A]I've made you sad,[N]haven't I? How stupid[N]of me… I'm sorry…[END]`
 
-widestring_04A04C `[TPL:1][TPL:A][NAM:1]: In this house,[N]there's no piano either,[N]huh?[FIN][TPL:0][TPL:B][NAM:0]: There isn't anything[N]like that! But, Grandma[N][NAM:6] is amazingly good[N]at singing.[FIN][CLD][::][TPL:1][TPL:A][NAM:1]: Speaking of[N]singing, they're singing[N]upstairs right now.[FIN]The two of them have loud[N]voices.[END]`
+widestring_04A04C `[TPL:1][TPL:9][NAM:1]: Anyway, is there[N]not a piano in this house?[FIN][CLD][PAU:0F][TPL:0][TPL:A][NAM:0]: No, I'm afraid not.[N]My Grandma [NAM:6] is an[N]incredible singer, though.[FIN][CLD][PAU:0F][::][TPL:1][TPL:B][NAM:1]: Speaking of[N]singing, both of your[N]grandparents are upstairs,[N]doing just that.[FIN][CLD][TPL:1][TPL:9]They have such loud[N]voices![END]`
 widestring_04A0D2 `[JMP:&widestring_04A04C+M]`
 
 ------------------------
 ?INCLUDE 'sc06_lola'
 ------------------------
-widestring_049ABB `[TPL:3][TPL:B][NAM:6]: Welcome home, [NAM:0].[N]I suppose that when I[N]sing opera, I forget what[N]time it is…[FIN][CLD][TPL:3][TPL:8]Dinner isn't ready.[END]`
+widestring_049ABB `[TPL:3][TPL:A][NAM:6]: Welcome home, [NAM:0].[N]Silly me. I lose track of[N]time when singing opera.[FIN][CLD][TPL:3][TPL:8]Dinner isn't ready yet.[END]`
 
 ------------------------
 ?INCLUDE 'sc06_bill'
 ------------------------
-widestring_0494B1 `[TPL:4][TPL:A]Bill: Oh dear. It's been[N]a while since I've sung[N]so loudly.[FIN]Your Grandma [NAM:6] was[N]once a singer for a bar.[FIN][TPL:B]Your grandma's beautiful[N]voice engraved itself on[N]my heart, so I proposed.[N]Bwahahaha.[END]`
+widestring_0494B1 `[TPL:4][TPL:A]Bill: Good gracious. It's[N]been a while since I sang[N]so loudly.[FIN]Long ago, your Grandma[N][NAM:6] was a songstress in[N]a bar, you know.[FIN]I fell in love with her[N]beautiful voice and heart,[N]so I proposed. Bwahahaha.[END]`
 
-widestring_049A3F `[TPL:3][TPL:A][NAM:6]: Ohohohoho. Stop[N]it, dear! Bringing up[N]that kind of thing now…[FIN]That's right! [NAM:0], you[N]shouldn't be shocked.[FIN]There was a girl here[N]singing with us a while[N]ago, right…?[END]`
+widestring_049A3F `[TPL:3][TPL:A][NAM:6]: Ohohohoo! Stop it,[N]dear! Bringing up a thing[N]like that right now…[FIN][CLD][PAU:0F][TPL:3][TPL:9]Oh yes! [NAM:0], try not to[N]be too surprised.[FIN][TPL:A]Until just a moment ago,[N]there was a girl here[N]singing with us, you know…[END]`
 
-widestring_04953E `[TPL:1][BOR:2][TPL:8][DLY:0]Noooooooooo…!!![FIN][CLD][TPL:9][SFX:10][DLY:0]A shriek echoed from the[N]first floor![END]`
+widestring_04953E `[TPL:1][BOR:2][TPL:8][DLY:0]No-o-o-o-o!!![FIN][CLD][TPL:9][SFX:10][DLY:0]A shriek resounded from[N]the first floor![END]`
 
-widestring_04956D `[TPL:4][TPL:A]Bill: That shriek was[N]from that child from a[N]moment ago…!![END]`
+widestring_04956D `[TPL:4][TPL:9]Bill: That sounds like her[N]screaming right now![END]`
 
 ------------------------
 ?INCLUDE 'sc06_main_soldier'
 ------------------------
-widestring_04A3BB `[TPL:19][TPL:A]Soldier: Your Royal[N]Highness. We've been[N]searching for you![FIN][TPL:1][BOR:2][TPL:A][NAM:1]: I don't know[N]anyone like you…[N]I said, leave…![FIN][CLD][TPL:19][TPL:B][SFX:10]Soldier: What are you[N]saying. If I don't bring[N]you home from here, my[N]head's going to roll.[END]`
+widestring_04A3BB `[TPL:19][TPL:9]Soldier: Your Highness.[N]We were looking for you![FIN][CLD][PAU:0F][TPL:1][BOR:2][TPL:A][NAM:1]: I don't know who[N]any of you people are.[N]Get out of here![FIN][CLD][PAU:0F][TPL:19][TPL:B][SFX:10]Soldier: What are you[N]saying? If I don't bring[N]you you back home, my[N]head's going to roll.[END]`
 
-widestring_04A0D7 `[TPL:1][TPL:B][NAM:1]: I think you've[N]got the wrong person.[N]I'm a flower girl[N]called Popongo.[FIN][TPL:19][TPL:B]Soldier: Your Highness![N]Do you think I can be[N]tricked with that kind[N]of red-faced lie…?[FIN]Ultimately, this is the[N]king's order. Even if it's[N]only with brute force,[N]we're bringing you home![END]`
+widestring_04A0D7 `[TPL:7][BOR:3][TPL:B][NAM:1]: I think you have[N]the wrong person. You see,[N]I'm a flower girl called[N]Popongo.[FIN][CLD][PAU:0F][TPL:19][TPL:A]Soldier: Your Highness![N]Do you think I can be[N]fooled with such a…[FIN][CLD][TPL:7][BOR:7][SEP:1,77FD][SEP:2,564F][SEP:3,1000][SEP:13,1000][TPL:8]…red-faced lie…?![FIN][TPL:19][TPL:B]This is a direct order[N]from the king. Even if we[N]have to use brute force,[N]we're taking you home![END]`
 
-widestring_04A15A `[TPL:1][TPL:A][NAM:1]: I'm sorry for[N]lying to you. I'm [NAM:1][N]of Edward Castle.[FIN]I think you were called[N][NAM:0]? It feels like we've[N]met before.[FIN]For some reason, it seems[N]like we could become good[N]friends.[END]`
 
-widestring_04958A `[TPL:4][TPL:A]Bill: Jeez, she turned out[N]to be a girl who likes[N]jokes. Haw haw haw.[END]`
+widestring_04A15A `[TPL:1][TPL:B][NAM:1]: I'm sorry for[N]lying to you. I'm really[N][NAM:1], the princess of[N]Edward Castle.[FIN][CLD][TPL:1][TPL:A]You said your name is[N][NAM:0]? It doesn't feel like[N]the first time we've met.[FIN][CLD][TPL:1][TPL:9]It somehow seems like[N]we'll be good friends.[END]`
 
-widestring_049B18 `[TPL:3][TPL:B][NAM:6]: If we're talking[N]about Edward Castle, it[N]has a giant aqueduct[N]underground.[FIN][TPL:4][TPL:B]Bill: I was the person[N]who built that that[N]labyrinthine aqueduct,[N][NAM:0].[FIN][CLD][TPL:0][BOR:2][TPL:8][NAM:0]: Wha…! Really?[END]`
+widestring_04958A `[TPL:4][TPL:9]Bill: There goes a real[N]prankster. Bwahaha.[END]`
 
-widestring_0495C9 `[PAU:1E][TPL:4][TPL:A]Bill: It was because I[N]was an architect long[N]ago.[END]`
+widestring_049B18 `[TPL:3][TPL:A][NAM:6]: Speaking of Edward[N]Castle, a vast aqueduct[N]lies beneath it, you know.[FIN][TPL:A]And let me tell you[N]something: the person who[N]designed that labyrinth?[FIN][CLD][TPL:3][TPL:9]That would be your[N]grandfather, here.[FIN][CLD][PAU:0F][TPL:0][BOR:2][TPL:8][NAM:0]: Huh? Really?[END][CLD]`
 
-widestring_0495EB `[TPL:4][TPL:9]There is a jail in that[N]castle's underground.[FIN][TPL:B]So that it would not be[N]simple for prisoners to[N]escape, it turned into a [N]complex structure.[FIN]However, the complex state[N]of mind of the people that[N]are sent to the jail I [N]created every day…[END]`
+widestring_0495C9 `[PAU:0F][TPL:4][TPL:9]Bill: Long ago, I used to[N]be an architect.[END]`
 
-widestring_049B82 `[PAU:40][::][TPL:3][TPL:A][NAM:6]: Well, with that[N]painful story ending, we[N]can have dinner now.[FIN][TPL:B]I've baked a delicious[N]pie. So come on both of[N]you, let's hurry upstairs[N]before it gets cold.[END]`
+widestring_0495EB `[TPL:4][TPL:9]There's a prison built[N]beneath the castle.[FIN][TPL:A]It was designed to be a[N]complex maze, so prisoners[N]couldn't escape easily.[FIN][TPL:B]Every day, though, I feel[N]guilt over all the people[N]still being sent to the[N]jail that I made…[END]`
 
-widestring_049685 `[TPL:4][TPL:B]Bill: Hey [NAM:0]. Don't you[N]think Grandma [NAM:6]'s[N]only been cooking weird[N]things lately?[FIN]Last night was miso rice.[N]Before that, sashimi[N]curry. I can't stand[N]eating like this…[FIN]They say that when people[N]have grown old and can't[N]help but have questions[N]about their own lives,[FIN]they've started to go[N]senile, but…[FIN]Somehow your grandma[N]probably cannot bear the[N]troubles we can't[N]speak of…[END]`
+widestring_049B82 `[PAU:40][::][TPL:3][TPL:A][NAM:6]: Okay, that's enough[N]depressing talk. Dinner's[N]about ready. Let's eat.[FIN][TPL:B]I baked a delicious pie.[N]Please come along now,[N]both of you. We'll sit[N]at the table upstairs.[END]`
 
-widestring_04A4CC `[TPL:0][DLG:3,B][SIZ:D,3][NAM:0]: Ultimately, dinner[N]was meat pies topped with[N]lots of whipped cream…[FIN]I only ate one slice, but[N]Grandpa Bill endured and[N]even ate three slices.[FIN][SIZ:D,4]Then, that night, I had a[N]dream. A dream of going[N]on a trip around the[N]world with [NAM:1]…[END]`
+widestring_049685 `[TPL:4][TPL:B]Bill: Hey [NAM:0]. Don't you[N]think your Grandma [NAM:6]'s[N]cooking has been a bit odd[N]lately?[FIN]Last night[NAM:17][N]rice. Before that, it was[N][NAM:18]. I can't[N]stand eating like this…[FIN]People say that when you[N]get old and there are[N]problems you can't fix,[N]you start going senile…[FIN][CLD][TPL:4][TPL:A]But maybe she's anxious[N]and troubled by something[N]she can't tell us about…[END]`
+
+widestring_04A4CC `[TPL:0][DLG:3,B][SIZ:D,4]In the end, dinner was a[N][NAM:19] pie that was[N]absolutely loaded with[N]whipped cream…[FIN]I could only stomach a[N]small piece, but Grandpa[N]Bill powered through and[N]ate three slices.[FIN][CLD][TPL:0][DLG:3,B][SIZ:D,3]That night, I dreamt that[N][NAM:1] and I were traveling[N]around the world together…[END]`
 
 ------------------------
 ?INCLUDE 'sc06_monologue'
 ------------------------
-widestring_04A582 `[TPL:E]Then, the next morning. A[N]terrible affair had tried[N]to arise…[END]`
+widestring_04A582 `[TPL:0][TPL:E]And the next morning,[N]something terrible was[N]about to unfold…[END]`
 
-widestring_049828 `[TPL:4][TPL:B]Bill: Good morning. For[N]breakfast we're having[N]Grandma [NAM:6]'s pie[N]again… Ugh…[END]`
+widestring_049828 `[TPL:4][TPL:B]Bill: Good morning. I had[N]to eat more of Grandma[N][NAM:6]'s pie for breakfast…[N](heavy sigh)[END]`
 
-widestring_049BE5 `[TPL:3][TPL:A][NAM:6]: Good morning, [NAM:0].[N]A letter has arrived for[N]you from King Edward.[FIN][CLD][TPL:9]In the letter was written[N]something like this.[FIN][TPL:4][BOR:1][TPL:B]"Bring the crystal ring[N]from [NAM:9]'s personal[N]possessions to Edward[N]Castle.ˮ       King Edward[FIN][TPL:3][TPL:B][NAM:6]: From the time I[N]saw this letter, I've[N]been ticked off somehow.[FIN]That's it. [NAM:0]. I'll teach[N]you a good luck charm.[FIN]When you're worried, if[N]you whistle this, you can[N]definitely get through.[FIN][CLD][TPL:9][NAM:6] hummed a mysterious[N]melody.[END]`
+widestring_049BE5 `[TPL:3][TPL:A][NAM:6]: Good morning, [NAM:0].[N]A letter from King Edward[N]has come for you.[FIN][CLD][TPL:9]This is what was written[N]in the letter…[FIN][CLD][PAU:0F][TPL:4][BOR:7][DLG:3,D][SIZ:D,6]"Please bring the crystal[N] ring that was found in[N] [NAM:9]'s belongings to[N] Edward Castle."[N][N]             -King Edward[FIN][CLD][PAU:0F][TPL:3][TPL:A][NAM:6]: From the momoent I[N]saw this letter, I've had[N]a temper stirring.[FIN][TPL:B]Ah, that's it! [NAM:0], let[N]me teach you this melody.[N]It's a kind of charm for[N]good luck.[FIN]Whenever you find yourself[N]in a bind, if you play[N]this melody, I'm sure[N]you'll make it through.[FIN][CLD][PAU:0F][TPL:9][NAM:6] hummed a mysterious[N]melody.[END]`
 
-widestring_049D29 `[TPL:0][TPL:9][SFX:0][DLY:5]That was a charming[N]melody.[PAU:58][CLR][TPL:B]Although I must have been[N]hearing it for the first[N]time, somehow I was[N]experiencing a very[N]nostalgic feeling.[PAU:60][CLD][TPL:9][SFX:0][DLY:1]You learned [NAM:6]'s[N]Melody![PAU:40][CLD]`
+widestring_049D29 `[TPL:0][TPL:8][SFX:0][DLY:5]It was a wonderful melody.[FIN][CLD][PAU:58][TPL:0][SFX:0][DLY:5][TPL:B]Even though this was the[N]first time I'd heard it,[N]for some reason, it filled[N]me with a deep longing.[FIN][CLD][PAU:60][TPL:8][SFX:0][DLY:1]Learned [NAM:6]'s Melody![PAU:40][END]`
 
 widestring_049D94 `[TPL:3][TPL:8][NAM:6]: Go, and be careful.[END]`
 
-widestring_04977F `[TPL:4][TPL:A]Bill: Crystal ring…[N]Uhh, I don't think I've[N]even heard of that.[FIN][TPL:B]There wasn't anything[N]like that in the luggage[N]your dad, [NAM:9], left[N]behind, either…[FIN][CLD][TPL:4][TPL:9]Anyhow, will you try to[N]go to Edward Castle?[FIN][TPL:A]You may be able to meet[N]the princess from[N]yesterday. Bwahahaha.[END]`
+widestring_04977F `[TPL:4][TPL:A]Bill: A crystal ring? Hm…[N]I don't think I've even[N]heard of such a thing.[FIN][TPL:A]There wasn't anything like[N]that in the baggage left[N]behind by your father…[FIN][CLD][TPL:4][TPL:9]At any rate, why not go to[N]Edward Castle?[FIN][TPL:B]Perhaps you'll be able to[N]meet the princess from[N]yesterday again, as well.[N]Bwahahaha.[END]`
 
 ------------------------
 ?INCLUDE 'sc02_lance'
 ------------------------
-widestring_04B3DF `[TPL:4][TPL:A][NAM:4]: Today is a holiday[N]from the lessons at church[N]too…[FIN]I think there's somehow[N]nothing interesting to do.[END]`
+widestring_04B3DF `[TPL:4][TPL:B][NAM:4]: We get a break from[N]church lessons today. I[N]wonder if there's anything[N]interesting going on.[END]`
 
 ------------------------
 ?INCLUDE 'sc01_guard'
 ------------------------
-widestring_048612 `[TPL:16][TPL:F]Huh? You say that King[N]Edward summoned for you[N]to go to the castle?[N]Well, go and take care.[END]`
+widestring_048612 `[TPL:16][TPL:E]King Edward has summoned[N]you to the castle? Well,[N]just be careful.[END]`
 
-widestring_048647 `[TPL:16][TPL:8]Take care, huh?[END]`
+widestring_048647 `[TPL:16][TPL:8]Be careful, huh?[END]`
