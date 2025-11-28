298890fe3a11b58e67ecee151129e65d3674d726
ViewtifulSlayer <viewtifulslayer@gmail.com>
2025-11-28 01:44:36 -0500

    Update chapter02.asm
    - Refine dialogue translations
    - Add [CLD][PAU:1E] pauses between speakers
    - Update action symbols from ** to ()

--- modules/base/patches/chapter02.asm
+++ modules/base/patches/chapter02.asm
@@ -22,7 +22,7 @@ widestring_04B5B6 `[TPL:5][TPL:9][NAM:5]: Ahaha! I'm sure to[N]win again.[END]`
 ----------------------------
 widestring_04B9AF `[TPL:0][TPL:A][DLY:0]Suddenly, [NAM:3] came flying[N]in with an urgent look[N]on his face![WAI][CLD]`
 
-widestring_04B9F2 `[TPL:3][BOR:2][TPL:9][NAM:3]: *huff* *huff*[N]News! It's BIG news![FIN][TPL:A]Edward Castle's princess[N]has gone missing! They say[N]she came to this town![END]`
+widestring_04B9F2 `[TPL:3][BOR:2][TPL:9][NAM:3]: (huff huff)[N]News! It's BIG news![FIN][TPL:A]Edward Castle's princess[N]has gone missing! They say[N]she came to this town![END]`
 
 widestring_04BA55 `[TPL:4][TPL:B][NAM:4]: Dude, so what? With[N]you barging in here like[N]that, I thought something[N]much cooler had happened.[FIN]Besides, that "princess"[N]is just that selfish girl[N][NAM:1], yeah? Who cares[N]about someone like that?[END][TPL:3][TPL:A][NAM:3]: That's true, but[N]soldiers will come around[N]here looking for her.[FIN][TPL:B]The soldiers from Edward[N]Castle just look so cool,[N]don't they? I really want[N]to see them.[END]`
 
@@ -32,7 +32,7 @@ widestring_04B612 `[TPL:5][TPL:A][NAM:5]: I'm not really[N]interested in girls.[
 
 widestring_04BB59 `[TPL:3][TPL:A][NAM:3]: Dang, I thought[N]everyone was gonna be so[N]amazed…[END]`
 
-widestring_04B652 `[TPL:5][TPL:A][NAM:5]: Alright, since[N]everyone's here, what's[N]the plan for today?[FIN][CLD][TPL:3][TPL:9][NAM:3]: Hey, I wanna see[N]Tim's mysterious power.[FIN][TPL:B]Come on, didn't you show[N]us the other day? You can[N]move things without using[N]your hands.[END][TPL:4][TPL:A][NAM:4]: If I'm remembering[N]right, he moved the stone[N]statue in the cave's nook.[FIN][CLD][TPL:4][TPL:9]Show us one more time,[N][NAM:0].[END]`
+widestring_04B652 `[TPL:5][TPL:A][NAM:5]: Alright, since[N]everyone's here, what's[N]the plan for today?[FIN][CLD][PAU:1E][TPL:3][TPL:9][NAM:3]: Hey, I wanna see[N]Tim's mysterious power.[FIN][TPL:B]Come on, didn't you show[N]us the other day? You can[N]move things without using[N]your hands.[END][TPL:4][TPL:A][NAM:4]: If I'm remembering[N]right, he moved the stone[N]statue in the cave's nook.[FIN][CLD][TPL:4][TPL:9]Show us one more time,[N][NAM:0].[END]`
 
 widestring_04BB89 `[TPL:3][TPL:A][NAM:3]: You moved the statue[N]without going near it the[N]other day, right?[END]`
 
@@ -55,13 +55,13 @@ widestring_04B77C `[TPL:5][TPL:A][NAM:5]: No matter how many[N]times I see it, i
 ----------------------------
 widestring_04B00F `[TPL:16][BOR:0][TPL:8][NAM:0] picks up a card.[WAI][N][TPL:A]Beyond the shadow of a[N]doubt, it was the Ace of[N]Diamonds![END]`
 
-widestring_04B328 `[TPL:4][BOR:2][TPL:9][NAM:4]: Ahh. Right on the[N]money![END]`
+widestring_04B328 `[PAU:1E][TPL:4][BOR:2][TPL:9][NAM:4]: Ahh. Right on the[N]money![END]`
 
 widestring_04BC16 `[TPL:3][TPL:9][NAM:3]: Phew. I'm[N]speechless…[FIN][CLD][TPL:3][TPL:8]Hey, [NAM:5].[FIN][TPL:A]This sort of thing is[N]called extrasensory[N]perception, right?[END]`
 
 widestring_04B7DE `[TPL:5][TPL:A][NAM:5]: The meaning of[N]"extrasensory perception"[N]is…[FIN]just as the word says: the[N]ability to perceive things[N]beyond normal human sense.[FIN][TPL:B]We usually identify the[N]traditional five human[N]senses as sight, hearing,[N]taste, smell, and touch.[FIN][CLD][TPL:5][TPL:A]I'm thinking that[N]extrasensory perception[N]might be the sixth sense.[END]`
 
-widestring_04B344 `[TPL:4][TPL:A][NAM:4]: I don't understand[N]the stuff Professor [NAM:5][N]says all that well.[END]`
+widestring_04B344 `[PAU:1E][TPL:4][TPL:A][NAM:4]: I don't understand[N]the stuff Professor [NAM:5][N]says all that well.[END]`
 
 widestring_04B386 `[TPL:4][TPL:A][NAM:4]: Come on, [NAM:5].[N]While the cards are out,[N]let's play another game.[END]`
 
